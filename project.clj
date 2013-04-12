77dad7692213151fb9c935135797993276b45bab
David Barksdale <amatus.amongus@gmail.com>
2013-04-11 22:22:38 -0500

    New lein project created with cljs-kickoff.

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject gnunet-web "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring "1.1.8"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :hooks [leiningen.cljsbuild]
+  :source-paths ["src/clj"]
+  :cljsbuild { 
+    :builds {
+      :main {
+        :source-paths ["src/cljs"]
+        :compiler {:output-to "resources/public/js/cljs.js"
+                   :optimizations :simple
+                   :pretty-print true}
+        :jar true}}}
+  :main gnunet-web.server)
+
