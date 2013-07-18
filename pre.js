ae33c2fd0899ef483984432c75d2ea0d3247f3a2
David Barksdale <amatus.amongus@gmail.com>
2013-07-17 22:47:57 -0500

    More love for gnunet-peerinfo-service.
    * Mocked out pipe(), we will never actually use the the returned fds.
    
    * Added another vital config value.

--- pre.js
+++ pre.js
@@ -1,2 +1,12 @@
+gnunet_prerun = function() {
+	ENV.GNUNET_PREFIX = "/.";
+	_pipe = function(filedes) {
+		var fd0 = FS.createFileHandle({isDevice: true});
+		var fd1 = FS.createFileHandle({isDevice: true});
+		setValue(filedes, fd0, 'i32');
+		setValue(filedes + 4, fd1, 'i32');
+		return 0;
+	}
+}
 if (typeof(Module) === "undefined") Module = { preRun: [] };
-Module.preRun.push(function() { ENV.GNUNET_PREFIX = "/." });
+Module.preRun.push(gnunet_prerun);
