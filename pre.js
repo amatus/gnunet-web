197703e1a190633df2c000d23e3690e6148a7088
David Barksdale <amatus@amatus.name>
2013-11-07 20:59:20 -0600

    Forgot to remove the old Module.print definition.

--- pre.js
+++ pre.js
@@ -1,7 +1,6 @@
 // vim: set expandtab ts=2 sw=2:
 gnunet_prerun = function() {
   ENV.GNUNET_PREFIX = "/.";
-  Module['print'] = function(x) { self.postMessage(x); };
 }
 if (typeof(Module) === "undefined") Module = { preRun: [] };
 Module.preRun.push(gnunet_prerun);
