e78d98a8ad7543179392f24beb35077102ef1869
David Barksdale <amatus@amatus.name>
2014-06-23 19:26:36 -0500

    Update ring to handle HEAD requests

--- src/clj/gnunet_web/server.clj
+++ src/clj/gnunet_web/server.clj
@@ -1,5 +1,5 @@
 ;; server.clj - development web server
-;; Copyright (C) 2013  David Barksdale <amatus@amatus.name>
+;; Copyright (C) 2013,2014  David Barksdale <amatus@amatus.name>
 ;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -16,16 +16,22 @@
 
 (ns gnunet-web.server
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.resource :as resources]
+            [ring.middleware.resource :as resource]
+            [ring.middleware.head :as head]
+            [ring.middleware.content-type :as content-type]
+            [ring.middleware.not-modified :as not-modified]
             [ring.util.response :as response])
   (:gen-class))
 
 (defn handler [request]
   (response/redirect "/index.html"))
 
-(def app 
+(def app
   (-> handler
-    (resources/wrap-resource "public")))
+    (head/wrap-head)
+    (resource/wrap-resource "public")
+    (content-type/wrap-content-type)
+    (not-modified/wrap-not-modified)))
 
 (defn -main [& args]
   (jetty/run-jetty app {:port 3000}))
