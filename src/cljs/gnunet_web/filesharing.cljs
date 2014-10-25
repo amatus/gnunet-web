06494613aeb26643b5e2261fce3ae7593376d056
David Barksdale <amatus@amatus.name>
2014-10-25 13:21:39 -0500

    Use client lib for searching.

--- src/cljs/gnunet_web/filesharing.cljs
+++ src/cljs/gnunet_web/filesharing.cljs
@@ -1,4 +1,4 @@
-;; filesharing.cljs - filesharing service for gnunet-web website
+;; filesharing.cljs - filesharing client for gnunet-web website
 ;; Copyright (C) 2014  David Barksdale <amatus@amatus.name>
 ;;
 ;; This program is free software: you can redistribute it and/or modify
@@ -15,115 +15,115 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (ns gnunet-web.filesharing
-  (:require [gnunet-web.encoder :refer (encode-uint32)]
-            [gnunet-web.message :refer (encode-message parse-message-types)]
-            [gnunet-web.metadata :refer (parse-metadata)]
-            [gnunet-web.parser :refer (items parser parse-absolute-time
-                                       parse-uint32 parse-utf8 tail)]
-            [gnunet-web.service :refer (client-connect)])
-  (:require-macros [monads.macros :as monadic]))
+  (:require [cljs.core.async :refer (chan close!)]
+            [gnunet-web.extractor :as e])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def message-type-start-search 136)
-(def message-type-fs-put 138)
-(def block-type-u 9)
+(defn -uri-ksk-create
+  [query]
+  (js/ccallFunc
+    js/_GNUNET_FS_uri_ksk_create
+    "number"
+    (array "string" "number")
+    (array query 0)))
 
-(defn encode-start-search
-  [{:keys [options type anonymity-level target query]
-    :or {options 0
-         type 0
-         anonymity-level 0
-         target (repeat 32 0)}}]
-  (encode-message
-    {:message-type message-type-start-search
-     :message (concat
-                (encode-uint32 options)
-                (encode-uint32 type)
-                (encode-uint32 anonymity-level)
-                target
-                query)}))
+(defn -uri-destroy
+  [uri]
+  (js/ccallFunc
+    js/_GNUNET_FS_uri_destroy
+    "void"
+    (array "number")
+    (array uri)))
 
-(def parse-u-block
-  (monadic/do parser
-              [signature (items 64)
-               purpose (items 8)
-               verification-key (items 32)
-               data tail]
-              {:signature signature
-               :purpose purpose
-               :verification-key verification-key
-               :data data}))
+(defn -start-search
+  [ctx uri anonymity options cctx]
+  (js/ccallFunc
+    js/_GNUNET_FS_search_start
+    "number"
+    (array "number" "number" "number" "number" "number")
+    (array ctx uri anonymity options cctx)))
 
-(def parse-encrypted-u-block
-  (monadic/do parser
-              [update-identifier parse-utf8
-               uri parse-utf8
-               metadata parse-metadata]
-              {:update-identifier update-identifier
-               :uri uri
-               :metadata metadata}))
+(defn uri-pointer-to-string
+  [uri-pointer]
+  (let [uri-string-pointer (js/_GNUNET_FS_uri_to_string uri-pointer)
+        uri-string (js/Pointer_stringify uri-string-pointer)]
+    (js/_free uri-string-pointer)
+    uri-string))
 
-(defn unwords
-  [word-array]
-  (take (.-sigBytes word-array)
-        (mapcat #(list (bit-and 0xff (bit-shift-right % 24))
-                       (bit-and 0xff (bit-shift-right % 16))
-                       (bit-and 0xff (bit-shift-right % 8))
-                       (bit-and 0xff %))
-                (.-words word-array))))
+(defn metadata-iterator
+  [metadata cls plugin-name type format mime-type data data-size]
+  (swap! metadata conj
+         {:plugin-name (js/Pointer_stringify plugin-name)
+          :type type
+          :format format
+          :mime-type (js/Pointer_stringify mime-type)
+          :data (if (or (= format e/format-utf8)
+                        (= format e/format-string)) 
+                  (js/Pointer_stringify data)
+                  (.subarray js/HEAP8 data data-size))}))
 
-(defn decrypt
-  [data]
-  (let [ciphertext (js/CryptoJS.lib.CipherParams.create
-                     (clj->js
-                       {:ciphertext (js/CryptoJS.lib.WordArray.create data)}))
-        twofish-key (js/CryptoJS.enc.Hex.parse "9A89B29A3896E200279ADC010D7F70F7F9A0582190881A17825B87754CA95B81")
-        twofish-iv (js/CryptoJS.enc.Hex.parse "9E71233934B556C9D9E97609CD22FBC1")
-        plaintext (js/CryptoJS.TwoFish.decrypt
-                    ciphertext twofish-key
-                    (clj->js {:iv twofish-iv
-                              :mode js/CryptoJS.mode.CFB
-                              :padding js/CryptoJS.pad.NoPadding}))
-        ciphertext (js/CryptoJS.lib.CipherParams.create
-                     (clj->js {:ciphertext plaintext}))
-        aes-key (js/CryptoJS.enc.Hex.parse "8D2E569BB26A7F45A933941996DC43C2F2C141F055AD2C863EE9A66379D59B61")
-        aes-iv (js/CryptoJS.enc.Hex.parse "FBE06BD519F62556BE465A67B80777EC")
-        plaintext (js/CryptoJS.AES.decrypt
-                    ciphertext aes-key
-                    (clj->js {:iv aes-iv
-                              :mode js/CryptoJS.mode.CFB
-                              :padding js/CryptoJS.pad.NoPadding}))]
-    (unwords plaintext)))
+(defn parse-progress-search
+  [status info-pointer]
+  (conj
+    {:status status
+     :cctx (js/getValue (+ 4 info-pointer) "i32")}
+    (condp = status
+      :search-result (let [metadata (atom [])
+                           callback (partial metadata-iterator metadata)
+                           callback-pointer (js/Runtime.addFunction callback)]
+                       (js/_GNUNET_CONTAINER_meta_data_iterate
+                         (js/getValue (+ 32 info-pointer) "i32")
+                         callback-pointer)
+                       (js/Runtime.removeFunction callback-pointer)
+                       {:uri (uri-pointer-to-string
+                               (js/getValue (+ 36 info-pointer) "i32"))
+                        :metadata @metadata})
+      nil)))
 
-(def parse-fs-put
-  (with-meta
-    (monadic/do parser
-                [type parse-uint32
-                 expiration parse-absolute-time
-                 last-transmission parse-absolute-time
-                 transmission-count parse-uint32
-                 respect-offered parse-uint32
-                 data (condp = type
-                        block-type-u parse-u-block
-                        tail)]
-                {:type type
-                 :expiration expiration
-                 :last-transmission last-transmission
-                 :transmission-count transmission-count
-                 :respect-offered respect-offered
-                 :data data})
-    {:message-type message-type-fs-put}))
+(def status-search-start 17)
+(def status-search-result 21)
+(defn parse-progress-info
+  [info-pointer]
+  (let [status (js/getValue (+ 112 info-pointer) "i32")]
+    (condp = status
+      status-search-start (parse-progress-search :search-start info-pointer)
+      status-search-result (parse-progress-search :search-result info-pointer)
+      (js/console.warn "ignored status:" status))))
 
-(defn search
-  [{:keys [callback] :as args}]
-  (let [message-channel (js/MessageChannel.)]
-    (set! (.-onmessage (.-port1 message-channel))
-          (fn [event]
-            (let [message @((parse-message-types #{parse-fs-put})
-                              (.-data event))]
-              (if (coll? message)
-                (callback (:message (first message)))
-                (js/console.warn "Failed to parse:" (.-data event))))))
-    (client-connect "fs" "web app (search)"
-                    (.-port2 message-channel))
-    (.postMessage (.-port1 message-channel)
-                  (into-array (encode-start-search args)))))
+(defn progress-callback
+  [cls info-pointer]
+  (when-let [info (parse-progress-info info-pointer)]
+    ;; Since this is a function we registered we can call it with
+    ;; non-C-compatible typed arguments and they get passed strait through.
+    (js/Runtime.dynCall "vi" (:cctx info) (array info))
+    (:cctx info)))
+
+(def fs
+  (js/ccallFunc
+    js/_GNUNET_FS_start
+    "number"
+    (array "number" "string" "number" "number" "number" "array")
+    (array 0 "gnunet-web" (js/Runtime.addFunction progress-callback) 0 0
+           (array 0))))
+
+(defn start-search
+  [query anonymity]
+  (let [uri (-uri-ksk-create query)
+        ch (chan 1)
+        callback (fn [info] (go (>! ch info)))
+        callback-pointer (js/Runtime.addFunction callback)
+        search (-start-search fs uri anonymity 0 callback-pointer)]
+    (-uri-destroy uri)
+    {:ch ch
+     :callback-pointer callback-pointer
+     :search search}))
+
+(defn stop-search
+  [{:keys [ch callback-pointer search]}]
+  (js/ccallFunc
+    js/_GNUNET_FS_search_stop
+    "void"
+    (array "number")
+    (array search))
+  (js/Runtime.removeFunction callback-pointer)
+  (close! ch))
--- src/cljs/gnunet_web/metadata.cljs
+++ src/cljs/gnunet_web/metadata.cljs
@@ -15,77 +15,17 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (ns gnunet-web.metadata
-  (:require [gnunet-web.extractor :as e :refer (type-descriptions)]
-            [gnunet-web.parser :as p :refer (items parser parse-uint32)]
-            [goog.crypt :refer (utf8ByteArrayToString)]
-            [monads.core :as m :refer (get-state update-state)])
-  (:require-macros [monads.macros :as monadic]))
-
-(defn decompress
-  [x]
-  (js/pako.inflate x))
-
-(def parse-entry
-  (monadic/do parser
-              [type parse-uint32
-               format parse-uint32
-               data-size parse-uint32
-               plugin-name-length parse-uint32
-               mime-type-length parse-uint32]
-              {:type type
-               :format format
-               :data-size data-size
-               :plugin-name-length plugin-name-length
-               :mime-type-length mime-type-length}))
-
-(defn parse-entry-data
-  [{:keys [type format data-size plugin-name-length mime-type-length]}]
-  (monadic/do parser
-              [mime-type (items mime-type-length)
-               plugin-name (items plugin-name-length)
-               data (items data-size)]
-              {:type type
-               :format format
-               :data data
-               :plugin-name (utf8ByteArrayToString plugin-name)
-               :mime-type (utf8ByteArrayToString mime-type)}))
-
-(def compressed   0x80000000)
-(def version-mask 0x7FFFFFFF)
-(def entry-size 20)
-
-(def parse-metadata
-  (monadic/do parser
-              [version parse-uint32
-               :when (= 2 (bit-and version-mask version))
-               entry-count parse-uint32
-               size parse-uint32
-               _ (if (bit-and compressed version)
-                   (update-state decompress)
-                   (parser nil))
-               entries (p/repeat entry-count parse-entry)
-               ;; GNUNET_CONTAINER_meta_data_deserialize() allows extra data
-               ;; between entries and entry-data so we consume it here.
-               :let [residue (- size
-                               (* entry-size entry-count)
-                               (reduce + (map #(+ (:data-size %)
-                                                  (:plugin-name-length %)
-                                                  (:mime-type-length %))
-                                              entries)))]
-               _ (items residue)
-               entry-data (m/map parse-entry-data (reverse entries))]
-              entry-data))
+  (:require [gnunet-web.extractor :refer [metatype-to-string]]))
 
 (defn pretty-print
   [metadata]
   (apply str
          (interpose
            "\n"
-           (map (fn [{:keys [format type data]}]
-                  (when (or (= e/format-utf8 format)
-                            (= e/format-string format))
+           (map (fn [{:keys [type data]}]
+                  (when (string? data)
                     (str
-                      (first (get type-descriptions type))
+                      (metatype-to-string type)
                       ": "
-                      (utf8ByteArrayToString data))))
+                      data)))
                 metadata))))
