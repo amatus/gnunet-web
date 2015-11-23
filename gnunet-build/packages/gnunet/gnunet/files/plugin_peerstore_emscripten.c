649199c96172c526b07a698e7ceddd9bbffee496
David Barksdale <amatus@amatus.name>
2015-11-23 17:56:26 -0600

    Update FSF address

--- gnunet-build/packages/gnunet/gnunet/files/plugin_peerstore_emscripten.c
+++ gnunet-build/packages/gnunet/gnunet/files/plugin_peerstore_emscripten.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of GNUnet
- * (C) 2013-2014  Christian Grothoff (and other contributing authors)
+ * Copyright (C) 2013-2014  Christian Grothoff (and other contributing authors)
  *
  * GNUnet is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
@@ -14,8 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with GNUnet; see the file COPYING.  If not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
 /**
