a08675381220cadf62cede4017177f921e4e215b
David Barksdale <amatus@amat.us>
2020-01-19 18:41:56 -0600

    Use a live hostlist

--- build-gnunet.sh
+++ build-gnunet.sh
@@ -5,8 +5,8 @@ mkdir -p assets/js
 cd gnunet-build
 export PATH="$PATH":"$(pwd)/scripts"
 build -d
-cp sysroot/var/lib/gnunet/hostlist ../assets/
 cp sysroot/var/lib/gnunet/js/* ../assets/js/
 mv ../assets/js/client-lib.js ../src/js/
+wget -O ../assets/hostlist https://gnunet.io/hostlist
 
 # vim: set expandtab ts=2 sw=2:
